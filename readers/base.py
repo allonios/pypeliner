b4d7d75fefe9461f8ce80f98bfb3c15458b306e5
allonios <fareck.allony.mail@gmail.com>
2022-08-31 20:17:27 +0300

    feat: added default reader

--- readers/base.py
+++ readers/base.py
@@ -6,3 +6,11 @@ class BaseReader(metaclass=ABCMeta):
     @abstractmethod
     def read(self) -> Any:
         ...
+
+
+class DefaultReader(BaseReader):
+    def __init__(self, default_value: Any) -> None:
+        self.default_value = default_value
+
+    def read(self) -> Any:
+        return self.default_value
