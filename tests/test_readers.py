a6c55374869b2c322b53a05d5030ba0f110ffae1
allonios <fareck.allony.mail@gmail.com>
2023-01-30 22:52:55 +0400

    feat: added tests

---
+++ tests/test_readers.py
@@ -0,0 +1,103 @@
+import pytest
+
+from pypeliner.readers.bulk_readers import CSVFileReader, FileReader
+
+
+@pytest.fixture
+def file_reader():
+    return FileReader(file_path="tests/test_files/file_reader_test_file.txt")
+
+
+@pytest.fixture
+def csv_reader(request):
+    return CSVFileReader(**request.param)
+
+
+def test_file_reader(file_reader):
+    assert file_reader.read() == "the quick brown fox jumps over the lazy dog."
+
+
+@pytest.mark.parametrize(
+    ["input_params", "expected_result"],
+    [
+        [
+            {
+                "file_path": "tests/test_files/csv_reader_test_file.csv",
+                "detect_numbers": False,
+            },
+            {
+                "column a": ["1", "a", "11", "trans", "dddddd"],
+                "column b": ["2", "c", "d", "late", "ddddd"],
+                "column c": ["3", "n", "dd", "da", "dddd"],
+                "column d": ["4", "k", "14", "ting", "ddd"],
+                "column e": ["5", "d", "fff", "fast", "d"],
+            },
+        ],
+        [
+            {
+                "file_path": "tests/test_files/csv_reader_test_file.csv",
+                "load_titles_row": True,
+                "detect_numbers": True,
+            },
+            {
+                "column a": [1, "a", 11, "trans", "dddddd"],
+                "column b": [2, "c", "d", "late", "ddddd"],
+                "column c": [3, "n", "dd", "da", "dddd"],
+                "column d": [4, "k", 14, "ting", "ddd"],
+                "column e": [5, "d", "fff", "fast", "d"],
+            },
+        ],
+        [
+            {
+                "file_path": "tests/test_files/csv_reader_test_file.csv",
+                "load_titles_row": True,
+                "detect_numbers": True,
+                "columns_names": ["a", "b", "c", "d", "e"],
+            },
+            {
+                "a": [1, "a", 11, "trans", "dddddd"],
+                "b": [2, "c", "d", "late", "ddddd"],
+                "c": [3, "n", "dd", "da", "dddd"],
+                "d": [4, "k", 14, "ting", "ddd"],
+                "e": [5, "d", "fff", "fast", "d"],
+            },
+        ],
+        [
+            {
+                "file_path": "tests/test_files/csv_reader_test_file.csv",
+                "load_titles_row": False,
+                "detect_numbers": True,
+                "columns_names": ["a", "b", "c", "d", "e"],
+            },
+            {
+                "a": ["column a", 1, "a", 11, "trans", "dddddd"],
+                "b": ["column b", 2, "c", "d", "late", "ddddd"],
+                "c": ["column c", 3, "n", "dd", "da", "dddd"],
+                "d": ["column d", 4, "k", 14, "ting", "ddd"],
+                "e": ["column e", 5, "d", "fff", "fast", "d"],
+            },
+        ],
+        [
+            {
+                "file_path": "tests/"
+                "test_files/"
+                "csv_reader_test_file_semicolon_delimiter.csv",
+                "load_titles_row": False,
+                "detect_numbers": True,
+                "delimiter": ";",
+                "columns_names": ["a", "b", "c", "d", "e"],
+            },
+            {
+                "a": ["column a", 1, "a", 11, "trans", "dddddd"],
+                "b": ["column b", 2, "c", "d", "late", "ddddd"],
+                "c": ["column c", 3, "n", "dd", "da", "dddd"],
+                "d": ["column d", 4, "k", 14, "ting", "ddd"],
+                "e": ["column e", 5, "d", "fff", "fast", "d"],
+            },
+        ],
+    ],
+)
+def test_csv_reader(input_params, expected_result):
+    # noinspection PyArgumentList
+    reader = CSVFileReader(**input_params)
+    assert reader.read() == expected_result
---
+++ tests/test_runner.py
@@ -0,0 +1,60 @@
+from typing import Any
+
+import pytest
+
+from pypeliner.processors.base import BaseProcessor
+from pypeliner.readers.base import DefaultReader
+from pypeliner.runners.base import BaseRunner
+
+
+class Processor1(BaseProcessor):
+    def process(self, input_state: Any = None) -> Any:
+        super().process(input_state)
+        return self.state
+
+
+class Processor2(BaseProcessor):
+    def process(self, input_state: Any = None) -> Any:
+        super().process(input_state)
+        return self.state * 2
+
+
+class Processor3(BaseProcessor):
+    def process(self, input_state: Any = None) -> Any:
+        super().process(input_state)
+        return self.state
+
+
+@pytest.fixture
+def pipeline():
+    return BaseRunner(
+        processors=[
+            Processor1(),
+            Processor2(),
+            Processor3(),
+        ],
+        reader=DefaultReader("test_runner"),
+    )
+
+
+def test_run_pipeline_runner(pipeline):
+    assert pipeline.run() == "test_runnertest_runner"
+
+
+def test_processors_call(mocker, pipeline):
+    mocked_reader = mocker.patch.object(
+        pipeline.reader, "read", return_value="test_runner"
+    )
+
+    mocked_processor1 = mocker.patch.object(
+        pipeline.processors[0], "process", return_value="test_runner"
+    )
+    mocked_processor3 = mocker.patch.object(
+        pipeline.processors[2], "process", return_value="test_runner"
+    )
+
+    pipeline.run()
+
+    mocked_reader.assert_called_once()
+    mocked_processor1.assert_called_once_with("test_runner")
+    mocked_processor3.assert_called_once_with("test_runnertest_runner")
